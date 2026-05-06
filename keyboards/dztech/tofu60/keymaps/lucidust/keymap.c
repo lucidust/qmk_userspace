1435a4d475ecc2a3472986abe279b48e69e586d9
lucidust <lucidust@gmail.com>
2026-05-06 12:56:28 +0900

    Move mute key to N on Layer 3 for tofu60 lucidust

--- keyboards/dztech/tofu60/keymaps/lucidust/keymap.c
+++ keyboards/dztech/tofu60/keymaps/lucidust/keymap.c
@@ -113,9 +113,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [3] = LAYOUT_60_ansi_split_bs_rshift(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR, KC_NUM,  KC_MPRV, KC_MNXT, KC_MPLY, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR, KC_NUM,  KC_MPRV, KC_MNXT, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SCRL, KC_TRNS, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_TRNS,          KC_TRNS,
-        KC_TRNS,          KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS, KC_TRNS, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,          KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [4] = LAYOUT_60_ansi_split_bs_rshift(
